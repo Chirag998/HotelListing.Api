ec54108c68adbdd4e19290de67ffc56bd6029b93
Chirag Gupta <chiragg.contractor@atlashxm.com>
2025-04-28 21:52:04 +0530

    added database changes

--- HotelListing.Api/Data/Hotel.cs
+++ HotelListing.Api/Data/Hotel.cs
@@ -6,5 +6,7 @@ public class Hotel
     public string Address { get; set; }
     public string Name { get; set; }
     public double Rating { get; set; }
+    public int CountryId { get; set; }
+    public Country Country { get; set; }
 }
 
---
+++ HotelListing.Api/Data/HotelListingDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Api.Data
+{
+    public class HotelListingDbContext:DbContext
+    {
+        public HotelListingDbContext(DbContextOptions<HotelListingDbContext> dbContextOptions):base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
